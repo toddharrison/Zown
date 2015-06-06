31a3e1eb138e1bcfdf6c63a82ebd01026675a79f
Todd Harrison <angrymonkeyofdoom@gmail.com>
2015-06-06 15:46:07 -0600

    Release 0.0.7 emergency fix for player zown cache preventing moving after a teleport.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Zown
 
-Zown is designed to be a world management system that allows server administrators to subdivide their worlds into functional areas that can behave differently from other divisions. This ability can be extended and controlled from the administrator level down to the player level allowing players to create and manage their own protected areas based on the permissions provided by the server administrator. It has been designed to also serve as a foundational tool that other plugins could build off of for more specialized needs such as towns, specialized flags, adventure maps, or anything requiring specialized handling of player access to interact or build.
+(Z)one (own)ership is designed to be a world management system that allows server administrators to subdivide their worlds into functional areas that can behave differently from other divisions. This ability can be extended and controlled from the administrator level down to the player level allowing players to create and manage their own protected areas based on the permissions provided by the server administrator. It has been designed to also serve as a foundational tool that other plugins could build off of for more specialized needs such as towns, specialized flags, adventure maps, or anything requiring specialized handling of player access to interact or build.
 
 Zown also includes an optional template system to help organize and manage the individual zowns. Templates allow administrators to create as many default configurations as they like for their subdivisions and then apply them to new zowns for instant setup. A template providing default claim permissions for player created zowns is a good example of how they might be used. The administrator can predefine a starting setup for players along with restrictions for modifications the player can make to those zowns. 
 
 Through the template system, an administrator can push configuration changes to all associated zowns belonging to a template with one modification. A zown with an assigned template can also later be over-ridden if you decide you want a particular zown to differ slightly from the template.
 
-> Zown is currently in beta development and not yet ready for live environments. Although it should be unlikely at this point, development requirements could make database structure modifications necessary. Should the need arise release documentation will outline these modifications.
-
-Refer to the wiki for complete (ha!) documentation.
+Refer to the wiki for complete documentation.
--- src/main/java/com/goodformentertainment/canary/zown/listener/PlayerListener.java
+++ src/main/java/com/goodformentertainment/canary/zown/listener/PlayerListener.java
@@ -1,8 +1,6 @@
 package com.goodformentertainment.canary.zown.listener;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import net.canarymod.Canary;
@@ -34,112 +32,100 @@ import com.goodformentertainment.canary.zown.api.impl.Tree;
 
 public class PlayerListener implements PluginListener {
 	private final IZownManager zownManager;
-	// TODO: Use this map for player location
-	private final Map<String, Tree<? extends IZown>> playerZownMap;
 	private final Set<Player> pearlingPlayers;
 	
 	public PlayerListener(final IZownManager zownManager) {
 		this.zownManager = zownManager;
-		playerZownMap = new HashMap<String, Tree<? extends IZown>>();
 		pearlingPlayers = new HashSet<Player>();
 	}
 	
 	@HookHandler(priority = Priority.CRITICAL)
 	public void onPlayerMove(final PlayerMoveHook hook) {
 		final Player player = hook.getPlayer();
+		final Tree<? extends IZown> fromZownTree = zownManager.getZown(hook.getFrom());
+		final Tree<? extends IZown> targetZownTree = zownManager.getZown(hook.getTo());
 		
-		Tree<? extends IZown> zownTree = playerZownMap.get(player.getUUIDString());
-		if (zownTree == null) {
-			zownTree = zownManager.getZown(player.getLocation());
-			playerZownMap.put(player.getUUIDString(), zownTree);
-		} else {
-			final Tree<? extends IZown> targetZownTree = zownManager.getZown(hook.getTo());
+		// Check if the player is authorized to be in the target zown
+		if (!player.isOperator() && targetZownTree.getData().hasEntryExclusion(player)) {
+			final Location fromLoc = hook.getFrom();
+			final Point minPoint = targetZownTree.getData().getMinPoint();
+			final Point maxPoint = targetZownTree.getData().getMaxPoint();
 			
-			// Check if the player is authorized to be in the target zown
-			if (!player.isOperator() && targetZownTree.getData().hasEntryExclusion(player)) {
+			double fromX = fromLoc.getX();
+			double fromY = fromLoc.getY();
+			double fromZ = fromLoc.getZ();
+			
+			final double insideMinX = fromX - minPoint.x;
+			final double insideMaxX = maxPoint.x - fromX;
+			final double insideMinY = fromY - minPoint.y;
+			final double insideMaxY = maxPoint.y - fromY;
+			final double insideMinZ = fromZ - minPoint.z;
+			final double insideMaxZ = maxPoint.z - fromZ;
+			
+			final double bump = 1.0;
+			
+			final double insideX = Math.min(insideMinX, insideMaxX);
+			final double insideY = Math.min(insideMinY, insideMaxY);
+			final double insideZ = Math.min(insideMinZ, insideMaxZ);
+			
+			if (insideX < insideY && insideX < insideZ) {
+				if (insideMinX < insideMaxX) {
+					// Closer to min x
+					fromX -= bump;
+				} else {
+					// Closer to max x
+					fromX += bump;
+				}
+			} else if (insideY < insideZ) {
+				if (insideMinY < insideMaxY) {
+					// Closer to min y
+					fromY -= bump;
+				} else {
+					// Closer to max y
+					fromY += bump;
+				}
+			} else {
+				if (insideMinZ < insideMaxZ) {
+					// Closer to min z
+					fromZ -= bump;
+				} else {
+					// Closer to max z
+					fromZ += bump;
+				}
+			}
+			
+			// Position toward outside of the zown
+			player.teleportTo(fromX, fromY, fromZ, fromLoc.getPitch(), fromLoc.getRotation());
+		} else if (fromZownTree != targetZownTree) {
+			// Changed zown
+			final Boolean flag = fromZownTree.getData().getConfiguration()
+					.getFlag(Flag.playerexit.name());
+			if (!player.isOperator() && flag != null && !flag) {
 				final Location fromLoc = hook.getFrom();
-				final Point minPoint = targetZownTree.getData().getMinPoint();
-				final Point maxPoint = targetZownTree.getData().getMaxPoint();
-				
-				double fromX = fromLoc.getX();
-				double fromY = fromLoc.getY();
-				double fromZ = fromLoc.getZ();
-				
-				final double insideMinX = fromX - minPoint.x;
-				final double insideMaxX = maxPoint.x - fromX;
-				final double insideMinY = fromY - minPoint.y;
-				final double insideMaxY = maxPoint.y - fromY;
-				final double insideMinZ = fromZ - minPoint.z;
-				final double insideMaxZ = maxPoint.z - fromZ;
-				
-				final double bump = 1.0;
-				
-				final double insideX = Math.min(insideMinX, insideMaxX);
-				final double insideY = Math.min(insideMinY, insideMaxY);
-				final double insideZ = Math.min(insideMinZ, insideMaxZ);
-				
-				if (insideX < insideY && insideX < insideZ) {
-					if (insideMinX < insideMaxX) {
-						// Closer to min x
-						fromX -= bump;
-					} else {
-						// Closer to max x
-						fromX += bump;
+				hook.setCanceled();
+				// TODO: Remove when Canary fixes bug of popping up when move is cancelled
+				Canary.getServer().addSynchronousTask(new ServerTask(Canary.getServer(), 0) {
+					@Override
+					public void run() {
+						player.teleportTo(fromLoc);
 					}
-				} else if (insideY < insideZ) {
-					if (insideMinY < insideMaxY) {
-						// Closer to min y
-						fromY -= bump;
-					} else {
-						// Closer to max y
-						fromY += bump;
-					}
-				} else {
-					if (insideMinZ < insideMaxZ) {
-						// Closer to min z
-						fromZ -= bump;
-					} else {
-						// Closer to max z
-						fromZ += bump;
-					}
+				});
+			} else {
+				final IZown zown = fromZownTree.getData();
+				final IZown targetZown = targetZownTree.getData();
+				if (zown.getFarewellMessage() != null && !zown.getFarewellMessage().isEmpty()
+						&& !zown.getFarewellMessage().equals("null")) {
+					player.message(zown.getFarewellMessage());
 				}
-				
-				// Position toward outside of the zown
-				player.teleportTo(fromX, fromY, fromZ, fromLoc.getPitch(), fromLoc.getRotation());
-			} else if (zownTree != targetZownTree) {
-				// Changed zown
-				final Boolean flag = zownTree.getData().getConfiguration().getFlag(Flag.playerexit.name());
-				if (!player.isOperator() && flag != null && !flag) {
-					final Location fromLoc = hook.getFrom();
-					hook.setCanceled();
-					// TODO: Remove when Canary fixes bug of popping up when move is cancelled
-					Canary.getServer().addSynchronousTask(new ServerTask(Canary.getServer(), 0) {
-						@Override
-						public void run() {
-							player.teleportTo(fromLoc);
-						}
-					});
-				} else {
-					playerZownMap.put(player.getUUIDString(), targetZownTree);
-					final IZown zown = zownTree.getData();
-					final IZown targetZown = targetZownTree.getData();
-					// player.message("Zown " + zown.getName() + " to " + targetZown.getName());
-					if (zown.getFarewellMessage() != null && !zown.getFarewellMessage().isEmpty()
-							&& !zown.getFarewellMessage().equals("null")) {
-						player.message(zown.getFarewellMessage());
-					}
-					if (targetZown.getWelcomeMessage() != null && !targetZown.getWelcomeMessage().isEmpty()
-							&& !targetZown.getWelcomeMessage().equals("null")) {
-						player.message(targetZown.getWelcomeMessage());
-					}
-					// TODO send PlayerZownExit and PlayerZownEntry
-					// TODO allow for cancel of player move
+				if (targetZown.getWelcomeMessage() != null && !targetZown.getWelcomeMessage().isEmpty()
+						&& !targetZown.getWelcomeMessage().equals("null")) {
+					player.message(targetZown.getWelcomeMessage());
 				}
+				// TODO send PlayerZownExit and PlayerZownEntry
+				// TODO allow for cancel of player move
 			}
 		}
 	}
-	
-	// TODO observe tp and connect for zown change
 	
 	@HookHandler(priority = Priority.CRITICAL)
 	public void onProjectileHit(final ProjectileHitHook hook) {
@@ -154,9 +140,6 @@ public class PlayerListener implements PluginListener {
 		}
 		
 		if (target != null && target.isPlayer()) {
-			// TODO optimization
-			// final Player player = (Player) target;
-			// final Tree<? extends IZown> zownTree = playerZownMap.get(player.getUUIDString());
 			final Tree<? extends IZown> zownTree = zownManager.getZown(target.getLocation());
 			if (attacker == null) {
 				final Boolean flag = zownTree.getData().getConfiguration()
@@ -200,9 +183,6 @@ public class PlayerListener implements PluginListener {
 		final Entity target = hook.getStruckEntity();
 		
 		if (target.isPlayer()) {
-			// TODO optimization
-			// final Player player = (Player) target;
-			// final Tree<? extends IZown> zownTree = playerZownMap.get(player.getUUIDString());
 			final Tree<? extends IZown> zownTree = zownManager.getZown(target.getLocation());
 			final Boolean flag = zownTree.getData().getConfiguration().getFlag(Flag.playerimmune.name());
 			if (flag != null && flag) {
@@ -217,9 +197,6 @@ public class PlayerListener implements PluginListener {
 		final Entity attacker = hook.getAttacker();
 		
 		if (target.isPlayer()) {
-			// TODO optimization
-			// final Player player = (Player) target;
-			// final Tree<? extends IZown> zownTree = playerZownMap.get(player.getUUIDString());
 			final Tree<? extends IZown> zownTree = zownManager.getZown(target.getLocation());
 			if (attacker == null) {
 				final Boolean flag = zownTree.getData().getConfiguration()
